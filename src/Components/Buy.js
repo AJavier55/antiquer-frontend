c3cd07d93415201bc093a35619ed00079a56e33c
AJavier55 <aliciajavier5@gmail.com>
2022-03-31 16:03:10 -0400

    changes

--- src/Components/Buy.js
+++ src/Components/Buy.js
@@ -1,17 +1,17 @@
-// import React from "react"
+import React from "react"
 
-// const Buy = (props) => {
+const Buy = (props) => {
    
-//     const item = props.items.find((item) => item.id === props.purchase.item_id)
+    const item = props.items.find((item) => item.id === props.purchase.item_id)
 
-//    return (
-//        <div className="bought">
-//            <img src={item.image} alt={item.name} />
-//             <h5>{item.name}</h5>
-//             <h5>{item.price}</h5>
-//             <h5>{props.purchase.quantity}</h5>
-//        </div>
-//    )
-// }
+   return (
+       <div className="bought">
+           <img src={item.image} alt={item.name} />
+            <h5>{item.name}</h5>
+            <h5>{item.price}</h5>
+            <h5>{props.purchase.quantity}</h5>
+       </div>
+   )
+}
 
-// export default Buy
+export default Buy
